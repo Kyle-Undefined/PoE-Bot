b5ad16b96d263a3628eb100e54c77b487ddca7e8
Kyle Undefined <kyle.undefined.feels.so@live.com>
2018-06-06 19:36:42 -0400

    - Added a LevenshteinDistance function to the TagMadule - Fixed the Suggested piece of the TagModule, like tags show up when a tag isn't found - Fixes issue #3

--- Modules/TagModule.cs
+++ Modules/TagModule.cs
@@ -113,11 +113,10 @@
             {
                 if (Suggest)
                 {
-                    var Suggested = Context.Server.Tags.Where(
-                        x => x.Name.ToLower().Contains(Name.ToLower())).Select(
-                        x => x.Name).Take(5);
-                    _ = ReplyAsync($"Couldn't find tag `{Name}` {Extras.Cross} " +
-                       (Suggested.Any() ? $"Try these: {string.Join(", ", Suggested)}" : null));
+                    var LDTags = Context.Server.Tags.Where(x => LevenshteinDistance(Name.ToLower(), x.Name) < 5);
+                    var ContainTags = Context.Server.Tags.Where(x => x.Name.Contains(Name.ToLower())).Take(5);
+                    var Suggested = LDTags.Union(ContainTags);
+                    _ = ReplyAsync($"Couldn't find tag `{Name}` {Extras.Cross} {(Suggested.Any() ? $"\nTry these: {string.Join(", ", Suggested.Select(x => x.Name))}" : null)} ");
                 }
                 return false;
             }
@@ -125,7 +124,26 @@
         }
 
         bool NameCheck(string Name)
-            => new[] { "help", "about", "tag", "delete", "remove", "delete", "info", "modify", "update", "user", "list" }.Any(x
-                => Name.ToLower().Contains(x));
+            => new[] { "help", "about", "tag", "delete", "remove", "delete", "info", "modify", "update", "user", "list" }.Any(x => Name.ToLower().Contains(x));
+
+        // Honestly have no idea what this is, found it on SO from a "close string match" search
+        int LevenshteinDistance(string a, string b)
+        {
+            if (String.IsNullOrEmpty(a) || String.IsNullOrEmpty(b)) return 0;
+
+            int lengthA = a.Length;
+            int lengthB = b.Length;
+            var distances = new int[lengthA + 1, lengthB + 1];
+            for (int i = 0; i <= lengthA; distances[i, 0] = i++) ;
+            for (int j = 0; j <= lengthB; distances[0, j] = j++) ;
+
+            for (int i = 1; i <= lengthA; i++)
+                for (int j = 1; j <= lengthB; j++)
+                {
+                    int cost = b[j - 1] == a[i - 1] ? 0 : 1;
+                    distances[i, j] = Math.Min(Math.Min(distances[i - 1, j] + 1, distances[i, j - 1] + 1), distances[i - 1, j - 1] + cost);
+                }
+            return distances[lengthA, lengthB];
+        }
     }
 }
