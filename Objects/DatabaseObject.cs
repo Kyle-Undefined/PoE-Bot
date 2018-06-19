0eaf52b4d87e2841f20329d089d11f1e6e4d50ee
Kyle Undefined <kyle.undefined.feels.so@live.com>
2018-06-18 20:43:05 -0400

    - Removed the Incremental backup

--- Objects/DatabaseObject.cs
+++ Objects/DatabaseObject.cs
@@ -12,7 +12,7 @@
         public bool IsConfigCreated = false;
         public string FullBackup = "0 * * * *";
         public string URL = "http://127.0.0.1:4009";
-        public string IncrementalBackup = "0 2 * * *";
+        public string IncrementalBackup = "";
         public string BackupFolder { get => Directory.CreateDirectory("Backup").FullName; }
     }
 }
