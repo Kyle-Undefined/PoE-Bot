2f6451f0a114d988177b86926fb07cb0724f2700
Kyle Undefined <kyle.undefined.feels.so@live.com>
2018-07-07 12:35:57 -0400

    - Undoing of a change to the Bot Main

--- PoEBot.cs
+++ PoEBot.cs
@@ -16,40 +16,37 @@
     {
         private static async Task Main(string[] args)
         {
-            using (DiscordSocketClient discordSocketClient = new DiscordSocketClient(new DiscordSocketConfig
-            {
-                MessageCacheSize = 50,
-                AlwaysDownloadUsers = true,
-                LogLevel = LogSeverity.Warning
-            }))
-            {
-                IServiceCollection services = new ServiceCollection()
-                    .AddSingleton(discordSocketClient)
-                    .AddSingleton(new CommandService(new CommandServiceConfig
-                    {
-                        ThrowOnError = false,
-                        IgnoreExtraArgs = false,
-                        DefaultRunMode = RunMode.Sync,
-                        CaseSensitiveCommands = false
-                    }))
-                    .AddSingleton(new CommandService())
-                    .AddSingleton<HttpClient>()
-                    .AddSingleton<DatabaseHandler>()
-                    .AddSingleton<JobHandler>()
-                    .AddSingleton<EventHelper>()
-                    .AddSingleton<MainHandler>()
-                    .AddSingleton<InteractiveService>()
-                    .AddSingleton<Handlers.EventHandler>()
-                    .AddSingleton(new Random(Guid.NewGuid().GetHashCode()))
-                    .AddSingleton(x => x.GetRequiredService<DatabaseHandler>().Execute<ConfigObject>(Operation.Load, id: nameof(Config)));
+            IServiceCollection services = new ServiceCollection()
+                .AddSingleton(new DiscordSocketClient(new DiscordSocketConfig
+                {
+                    MessageCacheSize = 50,
+                    AlwaysDownloadUsers = true,
+                    LogLevel = LogSeverity.Warning
+                }))
+                .AddSingleton(new CommandService(new CommandServiceConfig
+                {
+                    ThrowOnError = false,
+                    IgnoreExtraArgs = false,
+                    DefaultRunMode = RunMode.Sync,
+                    CaseSensitiveCommands = false
+                }))
+                .AddSingleton(new CommandService())
+                .AddSingleton<HttpClient>()
+                .AddSingleton<DatabaseHandler>()
+                .AddSingleton<JobHandler>()
+                .AddSingleton<EventHelper>()
+                .AddSingleton<MainHandler>()
+                .AddSingleton<InteractiveService>()
+                .AddSingleton<Handlers.EventHandler>()
+                .AddSingleton(new Random(Guid.NewGuid().GetHashCode()))
+                .AddSingleton(x => x.GetRequiredService<DatabaseHandler>().Execute<ConfigObject>(Operation.Load, id: nameof(Config)));
 
-                ServiceProvider provider = services.BuildServiceProvider();
-                await provider.GetRequiredService<DatabaseHandler>().InitializeAsync();
-                await provider.GetRequiredService<MainHandler>().InitializeAsync();
-                await provider.GetRequiredService<Handlers.EventHandler>().InitializeAsync();
-                provider.GetRequiredService<JobHandler>().Initialize();
-                await Task.Delay(-1);
-            }
+            ServiceProvider provider = services.BuildServiceProvider();
+            await provider.GetRequiredService<DatabaseHandler>().InitializeAsync();
+            await provider.GetRequiredService<MainHandler>().InitializeAsync();
+            await provider.GetRequiredService<Handlers.EventHandler>().InitializeAsync();
+            provider.GetRequiredService<JobHandler>().Initialize();
+            await Task.Delay(-1);
         }
     }
 }
