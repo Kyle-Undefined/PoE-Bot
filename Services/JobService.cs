180948790ef4612861e627a85bf333bd3691f6d8
Kyle <kyle.undefined@outlook.com>
2018-12-08 16:09:42 -0500

    - Added error handling to the Mute job

--- Services/JobService.cs
+++ Services/JobService.cs
@@ -1,5 +1,6 @@
 ﻿namespace PoE.Bot.Services
 {
+    using Discord;
     using Discord.WebSocket;
     using FluentScheduler;
     using Microsoft.EntityFrameworkCore;
@@ -14,14 +15,16 @@
         private readonly DiscordSocketClient _client;
         private readonly DatabaseContext _database;
         private readonly LeaderboardService _leaderboard;
+        private readonly LogService _log;
         private readonly RssService _rss;
         private readonly StreamService _stream;
 
-        public JobService(DiscordSocketClient client, DatabaseContext database, LeaderboardService leaderboard, RssService rss, StreamService stream)
+        public JobService(DiscordSocketClient client, DatabaseContext database, LeaderboardService leaderboard, LogService log, RssService rss, StreamService stream)
         {
             _client = client;
             _database = database;
             _leaderboard = leaderboard;
+            _log = log;
             _rss = rss;
             _stream = stream;
 
@@ -32,24 +35,32 @@
         {
             JobManager.AddJob(async () =>
             {
-                foreach (var mute in await _database.Users.Include(x => x.Guild).Where(x => x.Muted && x.MutedUntil < DateTime.Now).ToListAsync())
+                try
                 {
-                    mute.Muted = false;
-                    mute.MutedUntil = default;
-                    await _database.SaveChangesAsync();
+                    foreach (var mute in await _database.Users.Include(x => x.Guild).Where(x => x.Muted && x.MutedUntil < DateTime.Now).ToListAsync())
+                    {
+                        mute.Muted = false;
+                        mute.MutedUntil = default;
+                        await _database.SaveChangesAsync();
 
-                    var guild = _client.GetGuild(mute.Guild.GuildId);
-                    var user = guild.GetUser(mute.UserId);
-                    var role = guild.GetRole(mute.Guild.MuteRole) ?? guild.Roles.FirstOrDefault(x => x.Name is "Muted");
+                        var guild = _client.GetGuild(mute.Guild.GuildId);
+                        var user = guild.GetUser(mute.UserId);
+                        var role = guild.GetRole(mute.Guild.MuteRole) ?? guild.Roles.FirstOrDefault(x => x.Name is "Muted");
 
-                    if (user is null)
-                        return;
+                        if (user is null)
+                            return;
 
-                    if (!user.Roles.Contains(role))
-                        return;
+                        if (!user.Roles.Contains(role))
+                            return;
 
-                    if (user.Roles.Contains(role))
-                        await user.RemoveRoleAsync(role);
+                        if (user.Roles.Contains(role))
+                            await user.RemoveRoleAsync(role);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    await _log.LogMessage(new LogMessage(LogSeverity.Error, "Mute", string.Empty, ex));
+                    return;
                 }
             }, x => x.ToRunEvery(1).Minutes());
 
